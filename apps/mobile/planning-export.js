dbd1f1ec52e2ff19ddb3739adaa8812bb789ec2d
Rastaaslan <44372499+Rastaaslan@users.noreply.github.com>
2026-09-11 13:17:00 +0200

    compat: avoid Canvas roundRect dependency on mobile export

--- apps/mobile/planning-export.js
+++ apps/mobile/planning-export.js
@@ -15,8 +15,14 @@ function label(item) {
 }
 
 function rounded(ctx, x, y, width, height, radius) {
+  const r = Math.min(radius, width / 2, height / 2);
   ctx.beginPath();
-  ctx.roundRect(x, y, width, height, radius);
+  ctx.moveTo(x + r, y);
+  ctx.arcTo(x + width, y, x + width, y + height, r);
+  ctx.arcTo(x + width, y + height, x, y + height, r);
+  ctx.arcTo(x, y + height, x, y, r);
+  ctx.arcTo(x, y, x + width, y, r);
+  ctx.closePath();
 }
 
 function safeName(value) {
