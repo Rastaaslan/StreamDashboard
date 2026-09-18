8d84cc02f9767c6f867f3e50dcf0732b27c03ab4
Rastaaslan <44372499+Rastaaslan@users.noreply.github.com>
2026-09-18 09:22:49 +0200

    test(android): cover strict deep-link routing without Android stubs

--- android/app/src/test/java/com/rastaaslan/streamdashboard/remote/DeepLinkRouterTest.java
+++ android/app/src/test/java/com/rastaaslan/streamdashboard/remote/DeepLinkRouterTest.java
@@ -2,27 +2,31 @@ package com.rastaaslan.streamdashboard.remote;
 
 import static org.junit.Assert.assertEquals;
 
-import android.net.Uri;
 import org.junit.Test;
 
 public class DeepLinkRouterTest {
   @Test public void routesPairOnly() {
-    assertEquals(DeepLinkRouter.Route.PAIR, DeepLinkRouter.route(Uri.parse("streamdashboard://pair?v=1&id=abc")));
+    assertEquals(DeepLinkRouter.Route.PAIR, DeepLinkRouter.route("streamdashboard://pair?v=1&id=abc"));
   }
 
   @Test public void routesOAuthOnly() {
-    assertEquals(DeepLinkRouter.Route.OAUTH, DeepLinkRouter.route(Uri.parse("streamdashboard://oauth?provider=twitch&code=x&state=y")));
+    assertEquals(DeepLinkRouter.Route.OAUTH, DeepLinkRouter.route("streamdashboard://oauth?provider=twitch&code=x&state=y"));
   }
 
   @Test public void rejectsForeignSchemeAndHost() {
-    assertEquals(DeepLinkRouter.Route.INVALID, DeepLinkRouter.route(Uri.parse("https://pair")));
-    assertEquals(DeepLinkRouter.Route.INVALID, DeepLinkRouter.route(Uri.parse("streamdashboard://other")));
+    assertEquals(DeepLinkRouter.Route.INVALID, DeepLinkRouter.route("https://pair"));
+    assertEquals(DeepLinkRouter.Route.INVALID, DeepLinkRouter.route("streamdashboard://other"));
   }
 
   @Test public void rejectsUnexpectedPathFragmentPortOrUserInfo() {
-    assertEquals(DeepLinkRouter.Route.INVALID, DeepLinkRouter.route(Uri.parse("streamdashboard://pair/extra")));
-    assertEquals(DeepLinkRouter.Route.INVALID, DeepLinkRouter.route(Uri.parse("streamdashboard://pair#fragment")));
-    assertEquals(DeepLinkRouter.Route.INVALID, DeepLinkRouter.route(Uri.parse("streamdashboard://pair:1234")));
-    assertEquals(DeepLinkRouter.Route.INVALID, DeepLinkRouter.route(Uri.parse("streamdashboard://user@pair")));
+    assertEquals(DeepLinkRouter.Route.INVALID, DeepLinkRouter.route("streamdashboard://pair/extra"));
+    assertEquals(DeepLinkRouter.Route.INVALID, DeepLinkRouter.route("streamdashboard://pair#fragment"));
+    assertEquals(DeepLinkRouter.Route.INVALID, DeepLinkRouter.route("streamdashboard://pair:1234"));
+    assertEquals(DeepLinkRouter.Route.INVALID, DeepLinkRouter.route("streamdashboard://user@pair"));
+  }
+
+  @Test public void rejectsMalformedInput() {
+    assertEquals(DeepLinkRouter.Route.INVALID, DeepLinkRouter.route("%%%"));
+    assertEquals(DeepLinkRouter.Route.INVALID, DeepLinkRouter.route(""));
   }
 }
